ed810f233877fe356d68d178149ef04616fffa1d
Utilka <andrii.v.bolshakov@gmail.com>
2020-09-26 15:30:49 +0300

    more small bugfixes

--- sortedListNoDubl.py
+++ sortedListNoDubl.py
@@ -3,29 +3,29 @@ from myBisect import bisect
 
 class SortedListNoDubl:
 
-    def __init__(self, init_list=None, ascending_order=True):
+    def __init__(self, init_list=None, ascending_order=True, ignore_dub_flag=True):
         self.__array = []
         self.__ascending_order = ascending_order
 
         if init_list is not None:
-            self.add_unsorted_list(init_list)
+            self.add_unsorted_list(init_list,ignore_dub_flag=ignore_dub_flag)
 
 
-    def add(self, ad_item, ignore_flag=False, low=0, high=None):
+    def add(self, ad_item, ignore_dub_flag=True, low=0, high=None):
         if ad_item in self.__array:
-            assert not ignore_flag, "item already in list"
+            assert ignore_dub_flag, "item already in list"
             return
 
         ind = bisect(self.__array, ad_item, self.__ascending_order, low, high)
         self.__array.insert(ind, ad_item)
 
 
-    def add_unsorted_list(self, ad_list, ignore_flag=False):
+    def add_unsorted_list(self, ad_list, ignore_dub_flag=True):
         for i in ad_list:
-            self.add(i, ignore_flag)
+            self.add(i, ignore_dub_flag=ignore_dub_flag)
 
 
-    def merge_with_sorted_list(self, ad_list, ignore_flag=True):
+    def merge_with_sorted_list(self, ad_list, ignore_dub_flag=True):
         low = 0
 
         for ad_item in ad_list:
@@ -33,7 +33,7 @@ class SortedListNoDubl:
             low = ind
 
             if self.__array[ind] == ad_item:
-                assert not ignore_flag, "item already in list"
+                assert ignore_dub_flag, "item already in list"
             else:
                 self.__array.insert(ind, ad_item)
 
--- unitTest.py
+++ unitTest.py
@@ -5,13 +5,12 @@ from sortedListNoDubl import SortedListNoDubl
 
 class TestArr(unittest.TestCase):
 
-    def common_result_eq_test(self, cases, results,test=""):
+    def common_result_eq_test(self, cases, results, test=""):
         for a, r in zip(cases, results):
             with self.subTest(case=a):
                 b = SortedListNoDubl(list(a))
                 self.assertTrue(all(x == y for x, y in zip(b.get_list(), r)),
                                 msg=test + ": elements changed. a = " + str(a) + ", b = " + str(b) + ", r = " + str(r))
-
 
     def test_sorting(self):
         self.cases = ([], [0], [1], list(range(0, 10)), list(range(10, 0, -1)),
@@ -20,7 +19,7 @@ class TestArr(unittest.TestCase):
         self.results = ([], [0], [1], list(range(0, 10)), list(range(1, 11)),
                         [], [0], [-1], list(range(-10, 0)), list(range(-9, 1)))
 
-        self.common_result_eq_test(self.cases, self.results,'test_sorting')
+        self.common_result_eq_test(self.cases, self.results, 'test_sorting')
 
     def test_reverse_sorting(self):
         self.cases = ([], [0], [1], list(range(0, 10)), list(range(10, 0, -1)),
@@ -31,9 +30,10 @@ class TestArr(unittest.TestCase):
 
         for a, r in zip(self.cases, self.results):
             with self.subTest(case=a):
-                b = SortedListNoDubl(list(a))
+                b = SortedListNoDubl(list(a),ascending_order=False)
                 self.assertTrue(all(x == y for x, y in zip(b.get_list(), r)),
-                                msg= "test_reverse_sorting : elements changed. a = " + str(a) + ", b = " + str(b) + ", r = " + str(r))
+                                msg="test_reverse_sorting : elements changed. a = " + str(a) + ", b = " + str(
+                                    b) + ", r = " + str(r))
 
     def test_duplicates_removal(self):
         self.cases = ([1, 1], [1, 1, 2, 2, 3, 3, 4, 4],
@@ -44,7 +44,7 @@ class TestArr(unittest.TestCase):
                         [-1], [-4, -3, -2, -1],
                         list(range(0, 10)))
 
-        self.common_result_eq_test(self.cases, self.results,'test_duplicates_removal')
+        self.common_result_eq_test(self.cases, self.results, 'test_duplicates_removal')
 
     def test_universality(self):
         self.cases = ([4, 2, 8], list('abczzefgaa'), [True, False],
@@ -55,7 +55,7 @@ class TestArr(unittest.TestCase):
                         [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
                         [[1, 2], [2], [3, 4], [3, 4, 5], [6, 7]])
 
-        self.common_result_eq_test(self.cases, self.results,'test_universality')
+        self.common_result_eq_test(self.cases, self.results, 'test_universality')
 
     def test_merge(self):
         self.cases = ((list(range(0, 5)), list(range(5, 10))),
@@ -79,8 +79,8 @@ class TestArr(unittest.TestCase):
                                 msg="test_merge : elements changed. a = " + str(a) + ", b = " + str(b))
 
     def test_exceptions(self):
-        self.cases = (([1,2],["a","b"]),
-                      ([1,2],[1,2]))
+        self.cases = (([1, 2], ["a", "b"]),
+                      ([1, 2], [1, 2]))
 
         for a in self.cases:
             with self.subTest(case=a):
